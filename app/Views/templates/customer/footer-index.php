f195e6a6f1199882e73aa4b08c80fd3126667c0a
Rico <ferdianrico6@gmail.com>
2023-05-17 15:47:55 +0700

    footer customer

---
+++ app/Views/templates/customer/footer-index.php
@@ -0,0 +1,49 @@
+<div class="container-fluid footer p-3 mt-4">
+    <div class="row mb-4">
+        <div class="col-md-4 col-sm-4 col-xs-4 me-5">
+            <div class="footer-text pull-left">
+                <div class="d-flex">
+                    <img src="<?= base_url('logo-navbar.png'); ?>" alt="Logo" width="60%">
+                </div>
+                <p class="card-text card-text-footer mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi non pariatur numquam animi nam at impedit odit nisi.</p>
+                <ul class="list-group border-0">
+                    <li class="list-group-item border-0 bg-transparent"><a href="https://goo.gl/maps/XnhLAHCPiUrsvZZLA?coh=178572&entry=tt" target="_blank" class="list-card-text"><i class="bi bi-geo-alt-fill me-2"></i></i>Kota Tangerang Selatan, Banten, Indonesia</a></li>
+                    <li class="list-group-item border-0 bg-transparent"><a href="mailto:rajasafinishing@gmail.com" class="list-card-text"><i class="bi bi-envelope-fill me-2"></i></i>rajasafinishing@gmail.com</a></li>
+                    <li class="list-group-item border-0 bg-transparent"><a href="https://wa.me/622191542391" target="_blank" class="list-card-text"><i class="bi bi-whatsapp me-2"></i>(021) 91542391</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-2 mt-3">
+            <h5 class="heading">Products</h5>
+            <ul class="list-footer">
+                <li class="list-item-footer">Brochure</li>
+                <li class="list-item-footer">Calendar</li>
+                <li class="list-item-footer">Invitation</li>
+            </ul>
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-2 mt-3">
+            <h5 class="heading">Delivery</h5>
+            <ul class="list-footer">
+                <li class="list-item-footer">JNE</li>
+                <li class="list-item-footer">Pos Indonesia</li>
+                <li class="list-item-footer">J&T</li>
+                <li class="list-item-footer">TIKI</li>
+            </ul>
+        </div>
+        <div class="col-md-2 col-sm-2 col-xs-2 mt-3">
+            <h5 class="heading">Company</h5>
+            <ul class="list-footer">
+                <a href="" style="text-decoration: none; color: #fff">
+                    <li class="list-item-footer">About Us</li>
+                </a>
+            </ul>
+        </div>
+    </div>
+    <hr>
+    <div class="row" style="font-size:10px;">
+        <div class="col">
+            <p><i class="bi bi-c-circle"></i> <?= date('Y'); ?> RajasaFinishing</p>
+        </div>
+    </div>
+
+</div>
--- app/Views/templates/customer/index.php
+++ app/Views/templates/customer/index.php
@@ -15,11 +15,13 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
+
 </head>
 
 <body>
-    <?= $this->include('templates/customer/navbar-index') ?>
+    <?= $this->include('templates/customer/navbar-index'); ?>
     <?= $this->renderSection('content'); ?>
+    <?= $this->include('templates/customer/footer-index'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
